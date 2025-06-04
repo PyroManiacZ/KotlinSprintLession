85d753cb7641548db0805fd283447f2f1cc892f4
Nikolay <wolk20000@g,ail.com>
2025-06-04 15:28:27 +0300

    Initial Commit

--- src/main/kotlin/Lession_10.kt
+++ src/main/kotlin/Lession_10.kt
@@ -1,2 +1,16 @@
 package org.example
 
+fun main() {
+    printNameAndAge(userName = getName(), userAge = getAge())
+}
+
+fun getName() : String? = readLine()
+
+fun getAge() : Int? {
+    println("Введите возраст:")
+    return readLine()?.toInt()
+}
+
+fun printNameAndAge(userName: String?, userAge: Int?) {
+    println("Данные пользователя: $userName, $userAge лет")
+}
